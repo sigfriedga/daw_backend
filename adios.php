7503b356d4ce2de84974ed52b7c5ab05b4f20f0a
sigfried <sigfriedga@hotmail.com>
2020-11-13 19:00:30 +0100

    Caso3 git php

---
+++ adios.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caso2 adios php</title>
+</head>
+<body>
+
+<?php echo"Hasta luego desarrollador!!!" ?>
+ 
+</body>
+</html>
